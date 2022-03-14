c2677b8ca37f5d1a96fec4095f731ededdafcf73
ernesto <oberernst@gmail.com>
2022-03-13 23:05:03 -0400

    send ACK but echo the CON

--- lib/macrina/handler.ex
+++ lib/macrina/handler.ex
@@ -6,13 +6,12 @@ defmodule Macrina.Handler do
 
   @spec call(Connection.t(), Message.t()) :: :ok | {:error, term()}
   def call(%Connection{ip: ip, port: port, socket: socket}, %Message{type: :con} = message) do
+    Logger.info("#{port} sending ACK")
     ack = %Message{message | type: :ack}
-    Logger.info("#{port} echoing #{ack.type |> Atom.to_string() |> String.upcase()}")
     :gen_udp.send(socket, {ip, port}, Message.encode(ack))
 
-    non = %Message{message | message_id: Enum.random(10000..19999), type: :non}
-    Logger.info("#{port} echoing #{non.type |> Atom.to_string() |> String.upcase()}")
-    :gen_udp.send(socket, {ip, port}, Message.encode(non))
+    Logger.info("#{port} echoing CON")
+    :gen_udp.send(socket, {ip, port}, Message.encode(message))
   end
 
   def call(%Connection{ip: ip, port: port, socket: socket}, %Message{} = message) do
