02889736ae0a90442ef7f87ce606e2cf0df51ce9
Ernesto <oberernst@gmail.com>
2023-02-07 13:58:31 -0800

    msg response codes should default to incoming msg code

--- lib/macrina/message/message.ex
+++ lib/macrina/message/message.ex
@@ -25,7 +25,7 @@ defmodule Macrina.Message do
 
   def response(%__MODULE__{} = msg, opts \\ []) do
     %__MODULE__{
-      code: Keyword.get(opts, :code, :valid),
+      code: Keyword.get(opts, :code, msg.code),
       id: Keyword.get(opts, :id, Enum.random(10000..19999)),
       options: Keyword.get(opts, :options, []),
       payload: Keyword.get(opts, :payload, <<>>),
